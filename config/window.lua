85c17b8435a2d115ddaecba6e11cf48a61eed96b
rm-ryou <rmco.420118@gmail.com>
2025-12-10 01:18:01 +0900

    feat: add some window operation

--- config/window.lua
+++ config/window.lua
@@ -29,7 +29,18 @@ local function move_lower_half()
 	win:setFrame(rect)
 end
 
+local function max_size()
+	hs.window.focusedWindow():maximize()
+end
+
+local function toggle_full_screen()
+	hs.window.focusedWindow():toggleFullScreen()
+end
+
 hs.hotkey.bind(window_hot_key, "h", "Left harl of the Window", move_left_half)
 hs.hotkey.bind(window_hot_key, "j", "Lower harl of the Window", move_lower_half)
 hs.hotkey.bind(window_hot_key, "k", "Left harl of the Window", move_upper_half)
 hs.hotkey.bind(window_hot_key, "l", "Right harl of the Window", move_right_half)
+
+hs.hotkey.bind(window_hot_key, "m", "Maximize the Window", max_size)
+hs.hotkey.bind(window_hot_key, "f", "Toggle full screen the Window", toggle_full_screen)

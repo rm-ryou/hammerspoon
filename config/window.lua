939d14a3bb805c20981acc08765d6dd061a5e29d
rm-ryou <rmco.420118@gmail.com>
2025-12-10 00:58:19 +0900

    feat: basic window manage

---
+++ config/window.lua
@@ -0,0 +1,35 @@
+local hs = hs
+local window_hot_key = { "alt", "shift" }
+
+local function move_left_half()
+	local win = hs.window.focusedWindow()
+	local frame = win:screen():frame()
+	local rect = hs.geometry.rect(frame.x, frame.y, frame.w / 2, frame.h)
+	win:setFrame(rect)
+end
+
+local function move_right_half()
+	local win = hs.window.focusedWindow()
+	local frame = win:screen():frame()
+	local rect = hs.geometry.rect(frame.x + frame.w / 2, frame.y, frame.w / 2, frame.h)
+	win:setFrame(rect)
+end
+
+local function move_upper_half()
+	local win = hs.window.focusedWindow()
+	local frame = win:screen():frame()
+	local rect = hs.geometry.rect(frame.x, frame.y, frame.w, frame.h / 2)
+	win:setFrame(rect)
+end
+
+local function move_lower_half()
+	local win = hs.window.focusedWindow()
+	local frame = win:screen():frame()
+	local rect = hs.geometry.rect(frame.x, frame.y + frame.h / 2, frame.w, frame.h - frame.h / 2)
+	win:setFrame(rect)
+end
+
+hs.hotkey.bind(window_hot_key, "h", "Left harl of the Window", move_left_half)
+hs.hotkey.bind(window_hot_key, "j", "Lower harl of the Window", move_lower_half)
+hs.hotkey.bind(window_hot_key, "k", "Left harl of the Window", move_upper_half)
+hs.hotkey.bind(window_hot_key, "l", "Right harl of the Window", move_right_half)
--- init.lua
+++ init.lua
@@ -2,6 +2,7 @@ local hs = hs
 hs.loadSpoon("AClock")
 
 require("config.application")
+require("config.window")
 
 hs.hotkey.bind({ "cmd", "alt" }, "C", function()
 	spoon.AClock:toggleShow()
